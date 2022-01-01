1f07de9c7bdcae84a05bbc71cfd975fc3453e958
lst20082008 <sglst123@gmail.com>
2022-01-01 10:45:12 +0800

    function complete!

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from os import error
 import tkinter as tk
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk
+import os
 
 class Windows(Frame):
     def __init__(self, master):
         super().__init__(master)
         self.pack(fill=BOTH, expand=1)
         self.iFrames = []
+        self.outputPath = StringVar()
+        self.outputPath.set('Black')
+        self.outputWidth = StringVar()
+        self.outputHeight = StringVar()
+        self.outputWidth.set(100)
+        self.outputHeight.set(100)
+        self.outputImg = Image.new('L', (100, 100), color=0)
+        self.outputTkImg = ImageTk.PhotoImage(self.outputImg)
         
         self.topFrame = Frame(self)
         self.topFrame.grid(row=0, column=0)
@@ -16,12 +25,58 @@ class Windows(Frame):
         self.botFrame = Frame(self)
         self.botFrame.grid(row=1, column=0)
 
+        # add image frame to top frame
         num2str = ['r', 'g', 'b', 'a']
         for i in range(0, 4):
             iFrame = ImageFrame(self.topFrame)
             iFrame.grid(row=i, column=0)
             iFrame.ChangeLabel(num2str[i] + ' channel')
             self.iFrames.append(iFrame)
+        
+        self.outputLabel = Label(self.botFrame, image=self.outputTkImg)
+        self.outputEntry = Entry(self.botFrame, textvariable=self.outputPath)
+        self.outputFileBtn = Button(self.botFrame, text='save path', command=self.FindSavePath)
+        self.outputWidthLable = Label(self.botFrame, text='Width')
+        self.outputXEntry = Entry(self.botFrame, textvariable=self.outputWidth)
+        self.outputHeightLable = Label(self.botFrame, text='Height')
+        self.outputYEntry = Entry(self.botFrame, textvariable=self.outputHeight)
+        self.outputBtn = Button(self.botFrame, text='Generate', command=self.Generate)
+
+        self.outputLabel.pack()
+        self.outputEntry.pack()
+        self.outputFileBtn.pack()
+        self.outputWidthLable.pack()
+        self.outputXEntry.pack()
+        self.outputHeightLable.pack()
+        self.outputYEntry.pack()
+        self.outputBtn.pack()
+
+    def SetSize(self, width, height):
+        self.outputWidth.set(width)
+        self.outputHeight.set(height)
+    
+    def Generate(self):
+        self.GenerateImage()
+        self.SaveImg()
+
+    def GenerateImage(self):
+        channelImgs = []
+        for i in range(0,4):
+            outputSize = (int(self.outputWidth.get()), int(self.outputHeight.get()))
+            channelImgs.append(self.iFrames[i].image.resize(outputSize))
+        self.outputImg = Image.merge('RGBA', channelImgs)
+        self.outputTkImg = ImageTk.PhotoImage(self.outputImg.resize((100, 100)))
+        self.outputLabel.configure(image=self.outputTkImg)
+
+    def FindSavePath(self):
+        self.outputPath.set(filedialog.asksaveasfilename())
+
+    def SaveImg(self):
+        try:
+            self.outputImg.save(self.outputPath.get())
+            messagebox.showinfo('ok!', 'save complete!')
+        except BaseException as err:
+            messagebox.showerror('failed', f"save failed! {err=}")
 
 class ImageFrame(Frame):
     def __init__(self, master):
@@ -30,8 +85,6 @@ class ImageFrame(Frame):
         self.path.set('Black')
         self.image = Image.new('L', (100, 100), color=0)
         self.imageTk = ImageTk.PhotoImage(self.image)
-        #self.image = Image.open("testImg.jpg")
-        #self.image.resize((100, 100))
 
         self.textLabel = Label(self)
         self.textLabel.grid(row=0, column=0)
